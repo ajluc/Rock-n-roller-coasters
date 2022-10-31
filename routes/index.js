7f9a5407bcef70627238684b41c9ddc913980f86
Amaya Lucas <amayajlucas@gmail.com>
2022-10-31 15:51:36 -0400

    controllers and routes added, scratch file recreated

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const { Router } = require('express')
+const controllers = require('../controllers')
 const router = Router()
 
 router.get('/', (req, res) => res.send('Root'))
+
+router.post('/rollercoasters', controllers.createRollercoaster)
+router.get('/rollercoasters', controllers.getAllRollercoasters)
+router.delete('/rollercoasters', controllers.deleteRollercoaster)
+router.get('/rollercoasters/:id', controllers.getRollercoasterById)
 
 module.exports = router
