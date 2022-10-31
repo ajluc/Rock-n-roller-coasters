d4aee2f5af9f0c47825822bac180dd186b140d3c
David Caldwell <doubledc92@gmail.com>
2022-10-31 15:07:22 -0400

    adding 2 routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <main>
+        <Routes>
+          <Route path="" element="">
+            Home
+          </Route>
+          <Route path="" element="">
+            Rollercoasters
+          </Route>
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
