5a63c98399524b219129d6a8bbe027f9042b1b94
Jeff Carroll <jcarr048@gmail.com>
2022-11-01 10:11:28 -0400

    Got data to show up on home

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,7 +3,7 @@ const Card = (props) => {
     return (
         <div className="card" onClick={props.onClick(props.id)}>
             <div className="img-wrapper">
-                <img src={props.image} alt={props.name}></img>
+                <img src={props.img} alt={props.name}></img>
             </div>
             <div className="info-wrapper">
                 <h3>{props.name}</h3>
