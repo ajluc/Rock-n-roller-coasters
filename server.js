c2454b6a28828e6199791fcfe3f59e8d2c04729b
Amaya Lucas <amayajlucas@gmail.com>
2022-11-02 09:35:42 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -14,10 +14,16 @@ app.use(express.json())
 app.use(logger('dev'))
 app.use(cors())
 
+app.use(express.static(`${__dirname}/client/build`))
+
 // app.use() middleware ^^
 
 app.use('/', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
